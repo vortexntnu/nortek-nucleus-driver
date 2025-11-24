4be79c950e9f71b905b4263726eb9a24234292b3
forrisdahl <nathanif@stud.ntnu.no>
2025-11-24 20:16:22 +0100

    feat: add header structs

--- include/nortek_nucleus_messages.hpp
+++ include/nortek_nucleus_messages.hpp
@@ -1,4 +1,26 @@
 #ifndef NORTEK_NUCLEUS_MESSAGES_HPP_
 #define NORTEK_NUCLEUS_MESSAGES_HPP_
 
-#endif // NORTEK_NUCLEUS_MESSAGES_HPP_
+#include <cstdint>
+
+struct HeaderData {
+    uint8_t sync_byte;
+    uint8_t header_size;
+    uint8_t data_series_id;
+    uint8_t family_id;
+    uint16_t data_size;
+    uint16_t data_checksum;
+    uint16_t header_checksum;
+};
+
+struct CommonData {
+    uint8_t version;
+    uint8_t offset_data;
+    uint8_t flags;
+    uint8_t reserved[1];
+
+    uint32_t timestamp;
+    uint32_t microseconds;
+};
+
+#endif  // NORTEK_NUCLEUS_MESSAGES_HPP_
