14060071e6d9026d55a96971b32b2b4700aa75b7
forrisdahl <nathanif@stud.ntnu.no>
2025-11-25 16:32:57 +0100

    feat: add base class structure

--- include/nortek_nucleus_driver.hpp
+++ include/nortek_nucleus_driver.hpp
@@ -2,8 +2,25 @@
 #ifndef NORTEK_NUCLEUS_DRIVER_HPP_
 #define NORTEK_NUCLEUS_DRIVER_HPP_
 
+#include <asio.hpp>
+
+struct ConnectionParams {
+    std::string remote_ip;
+    uint16_t data_remote_port;
+    uint16_t data_local_port;
+};
+
+class NortekNucleusDriver {
+   public:
+    explicit NortekNucleusDriver(asio::io_context& io);
+
+    std::error_code open_tcp_sockets(const ConnectionParams& params);
 
 
 
 
-#endif // NORTEK_NUCLEUS_DRIVER_HPP_
+   private:
+    asio::ip::tcp::socket nucleus_sock_;
+};
+
+#endif  // NORTEK_NUCLEUS_DRIVER_HPP_
--- src/nortek_nucleus_driver.cpp
+++ src/nortek_nucleus_driver.cpp
@@ -1,6 +1,23 @@
 
 #include "nortek_nucleus_driver.hpp"
 
+NortekNucleusDriver::NortekNucleusDriver(
 
+    asio::io_context& io)
+    : nucleus_sock_(io) {}
 
+std::error_code NortekNucleusDriver::open_tcp_sockets(
+    const ConnectionParams& params) {
+    std::error_code ec;
+    auto addr = asio::ip::make_address(params.remote_ip, ec);
+    if (ec) {
+        return ec;
+    }
 
+    asio::ip::tcp::endpoint nucleus_endpoint(addr, params.data_remote_port);
+    nucleus_sock_.connect(nucleus_endpoint, ec);
+    if (ec) {
+        return ec;
+    }
+    return {};
+}
